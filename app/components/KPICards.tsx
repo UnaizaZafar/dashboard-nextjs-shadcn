c0cf569db0f2b00a8477c6f5fa71b9a4644200f9
UnaizaZafar <unaizaofficial840@gmail.com>
2025-11-05 14:19:04 +0500

    added responsive to cards

--- app/components/KPICards.tsx
+++ app/components/KPICards.tsx
@@ -143,7 +143,7 @@ export function KPICards() {
     );
   }
   return (
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:gap-8">
+    <div className="grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3 lg:gap-8">
       {allMetrics.map((metric) => (
         <Card key={metric.title}>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
